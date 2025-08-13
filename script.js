9447f241899ea2d3c115444601eb0002e2a0c015
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-08-13 10:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 //complete this code
 class Animal {
 	constructor(species) {
-    this._species = species;
-}
-	get species() {
-    return this._species;
-  }
+		this.species = species;
+	}
 
-  makeSound() {
-    console.log(`The ${this._species} makes a sound`);
-  }
-}
+	get speciesName() {
+		return this.species;
+	}
 
+	makeSound() {
+		console.log(`The ${this.species} makes a sound`);
+	}
+}
 
 class Dog extends Animal {
 	bark() {
-    console.log("woof");
-  }
-}
+		console.log("woof");
+	}
 }
 
 class Cat extends Animal {
-	 purr() {
-    console.log("purr");
-  }
+	purr() {
+		console.log("purr");
+	}
 }
-
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
